1f96da0e42d4a40c8c011db7a8eea180d3dfc141
comprel <1079926688@qq.com>
2019-10-07 21:39:08 +0800

    update cache redis

--- driver/CACHE/Redis.py
+++ driver/CACHE/Redis.py
@@ -1,7 +1,71 @@
 # coding:utf-8
 
+import redis
+from core.conf import CACHE_SERVICE
+from core.conf import CACHE_PORT
+from core.conf import CACHE_DB
+from core.conf import CACHE_PASSWORD
+from core.conf import CACHE_MAX_CONNECTION
 from driver.CACHE.base import CacheBase
+
+pool = redis.ConnectionPool(host=CACHE_SERVICE, port=CACHE_PORT, password=CACHE_PASSWORD,
+                            db=CACHE_DB, max_connections=CACHE_MAX_CONNECTION)
+redis_connector = redis.Redis(connection_pool=pool, socket_timeout=2)
 
 
 class Redis(CacheBase):
-    pass
+    @classmethod
+    def connector(cls):
+        return redis_connector
+
+    @classmethod
+    def get(cls, key):
+        try:
+            conn = cls.connector()
+            res = conn.get(key)
+            conn = None
+            return res
+        except:
+            return None
+
+    @classmethod
+    def set(cls, key, value, timeout=None):
+        try:
+            conn = cls.connector()
+            conn.set(name=key, value=value)
+            if timeout:
+                conn.expire(name=key, time=timeout)
+            conn = None
+            return True
+        except:
+            return False
+
+    @classmethod
+    def delete(cls, key):
+        try:
+            conn = cls.connector()
+            conn.delete(key)
+            conn = None
+            return True
+        except:
+            return False
+
+    @classmethod
+    def incr(cls, key, value):
+        try:
+            conn = cls.connector()
+            conn.incr(name=key, amount=value)
+            conn = None
+            return True
+        except:
+            return False
+
+    @classmethod
+    def decr(cls, key, value):
+        try:
+            conn = cls.connector()
+            conn.decr(name=key, amount=value)
+            conn = None
+            return True
+        except:
+            return False
--- driver/CACHE/base.py
+++ driver/CACHE/base.py
@@ -1,17 +1,26 @@
 # coding:utf-8
 
 class CacheBase(object):
-    def incr(self, key, value):
-        raise IndentationError("not define")
+    @classmethod
+    def incr(cls, key, value):
+        raise AttributeError("not define")
 
-    def decr(self, key, value):
-        raise IndentationError("not define")
+    @classmethod
+    def decr(cls, key, value):
+        raise AttributeError("not define")
 
-    def set(self, key, value, timeout=None):
-        raise IndentationError("not define")
+    @classmethod
+    def set(cls, key, value, timeout=None):
+        raise AttributeError("not define")
 
-    def get(self, key):
-        raise IndentationError("not define")
+    @classmethod
+    def delete(cls, key):
+        raise AttributeError("not define")
 
-    def get_int(self):
-        raise IndentationError("not define")
+    @classmethod
+    def get(cls, key):
+        raise AttributeError("not define")
+
+    @classmethod
+    def get_int(cls):
+        raise AttributeError("not define")
